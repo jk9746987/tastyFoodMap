870223c061fae3330b680a94827313c1ae9a3bd6
jk9746987 <jk9746987@gmail.com>
2023-07-09 23:23:18 +0800

    test

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,14 +1,9 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-    <button class="bg-red-500 hover:bg-blue-500 text-white p-3">
-      按鈕 1
-    </button>
-    <div class="bg-blue-500 text-white p-4">
-      歡迎使用 Tailwind CSS！
-      test
+    <div class="about">
+        <h1>This is an about page</h1>
+        <button class="bg-red-500 hover:bg-blue-500 text-white p-3">按鈕 1</button>
+        <div class="bg-blue-500 text-white p-4">歡迎使用 Tailwind CSS！ test test</div>
     </div>
-  </div>
 </template>
 
 <style>
@@ -16,10 +11,10 @@
 @import 'tailwindcss/components';
 @import 'tailwindcss/utilities';
 @media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
+    .about {
+        min-height: 100vh;
+        display: flex;
+        align-items: center;
+    }
 }
 </style>
