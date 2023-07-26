888d677ae60955bbf4c5b477a7704185c844cf02
jk9746987 <jk9746987@gmail.com>
2023-07-26 16:34:10 +0800

    test

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -3,6 +3,7 @@
         <h1>This is an about page</h1>
         <button class="bg-red-500 hover:bg-blue-500 text-white p-3">按鈕 1</button>
         <div class="bg-blue-500 text-white p-4">歡迎使用 Tailwind CSS！ test test</div>
+        <div>測試拿掉simplegithood資料夾能不能precommit</div>
     </div>
 </template>
 
